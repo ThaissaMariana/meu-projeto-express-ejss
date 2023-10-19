759216f5d03014ba427f3538e67b09b0c990f2bb
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-10-19 16:35:45 -0400

    Commit alt

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+ {id: 1, nome: "nome do produto", preco: "400", descricao: "Descricao do produto", imagem: "produto1.jpg"},
+]
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Produtos Hello Kitty!' });
 });
 
 app.get('/produto', (req, res) => {
-  res.render('produto', { message: 'Produtos Hello Kitty' });
+  res.render('produto', { message: 'Produtos Hello Kitty 2' });
+});
+
+app.get('/produto1', (req, res) => {
+  res.render('produto1', { message: 'Produtos Hello Kitty 2' });
+});
+
+app.get('/produto2', (req, res) => {
+  res.render('produto2', { message: 'Produtos Hello Kitty 2' });
+});
+
+app.get('/produto3', (req, res) => {
+  res.render('produto3', { message: 'Produtos Hello Kitty 2' });
 });
 
 app.listen(port, () => {
